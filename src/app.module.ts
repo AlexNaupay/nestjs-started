6de6dea1e4c12fbc8c44b72c57d9cec2bdf7446b
Alex Naupay <alexnaupay@gmail.com>
2024-09-03 23:22:26 -0500

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
 import { CategoriesController } from './categories/categories.controller';
-import { ProductsService } from './products/products.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-    imports: [],
-    controllers: [AppController, ProductsController, CategoriesController],
-    providers: [AppService, ProductsService],
+    imports: [ProductsModule],
+    controllers: [AppController, CategoriesController],
+    providers: [AppService],
 })
 export class AppModule {}
