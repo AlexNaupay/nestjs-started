89f36898079785e5424d733d96d842954df241d0
Alex Naupay <alexnaupay@gmail.com>
2024-09-08 14:59:13 -0500

    nodejs pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { Client } from 'pg';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesController } from './categories/categories.controller';
 import { ProductsModule } from './products/products.module';
-import { ConfigModule } from '@nestjs/config';
 import configuration from '../config/configuration';
+
+const client = new Client({
+    user: 'root',
+    password: '123456',
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'store_db',
+});
+
+client.connect();
+
+client.query('SELECT * FROM tasks', (err, res) => {
+    console.log(err);
+    console.log(res.rows);
+});
 
 @Module({
     imports: [ProductsModule, ConfigModule.forRoot({ load: [configuration], isGlobal: true })],
